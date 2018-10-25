c8f8c55d067feb66ad4d45e00a0faf94b65e69a8
[Sara Dyatlova-Murphy] <[saradyatlovamurphy@catalyst.net.nz]>
2018-10-26 08:38:01 +1300

    quick tidy

--- moviewatchlist/movies/views.py
+++ moviewatchlist/movies/views.py
@@ -8,14 +8,7 @@ def home(request):
 def movies_detail(request, pk):
     if request.method == "GET":
         movie = get_object_or_404(Movie, pk=pk)
-        # movie_id = Movie.objects.get(pk=id)
         # Filter list: get actors/actresses who have a movie with this pk.
         # .first(): Get the first performer off the list.
         actor = ActorActressList.objects.filter(movies__pk=pk).first()
-        # try:
-        #     movie = Movie.objects.get(pk=pk)
-        #     lead_in_movie = ActorActressList.objects.filter(movies__movie_title=lead)
-        # except Movie.DoesNotExist:
-        #     raise Http404
     return render(request, "movie_detail.html", {"movie": movie, "actor": actor})
-    # return render(request, "movie_detail.html", {"movie": movie})
