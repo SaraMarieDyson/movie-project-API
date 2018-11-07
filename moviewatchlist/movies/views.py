8aec7031c0680d7e637ae8fdba183872ccb82b40
[Sara Dyatlova-Murphy] <[saradyatlovamurphy@catalyst.net.nz]>
2018-11-08 10:15:11 +1300

    Have a very basic movie list page

--- moviewatchlist/movies/views.py
+++ moviewatchlist/movies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from . models import Movie, ActorActressList
+from . models import Movie, ActorActressList, MovieWatchList
+
 
 def home(request):
     movies = Movie.objects.all()
--- moviewatchlist/moviewatchlist/urls.py
+++ moviewatchlist/moviewatchlist/urls.py
@@ -1,3 +1,6 @@
+
+
+
 """moviewatchlist URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
